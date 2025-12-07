2f9cbe7c8a620f6ceedf8f8e6df1ff1f4cb51d67
Chase Crossley <ccross41815@gmail.com>
2025-12-07 01:41:33 -0600

    style: Update product grid layout

--- src/templates/ModernFarmTemplate.tsx
+++ src/templates/ModernFarmTemplate.tsx
@@ -86,7 +86,7 @@ const filteredProducts = categoryFiltered.sort((a, b) => {
 />
 
 
-          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 md:py-14 lg:py-16">
+          <div className="relative max-w-7xl mx-auto px-1 sm:px-3 md:px-5 lg:px-8 py-10 md:py-10 lg:py-14">
             <div className="max-w-2xl">
               {/* Only render heading if not an empty string */}
               {heroHeading && (
